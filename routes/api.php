6e9dfb8c386793d0f630cd21c952b29b471b0be2
yu <yu@gmail.com>
2021-03-11 23:31:16 +0800

    [add] 新增 api web

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\PublicController;
+use App\Http\Controllers\API\MessageController;
+use App\Http\Controllers\API\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::post('register', [PublicController::class, 'register']);
+Route::post('login', [PublicController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('test', [PublicController::class, 'test']);
+    Route::post('logout', [PublicController::class, 'logout']);
+
+    // 使用者編輯功能
+    Route::prefix('user')->group(function () {
+        Route::post('edit', [UserController::class, 'userEdit']);
+        // Route::post('password-edit', [UserController::class, 'passwordEdit']);
+    });
+
+    // 留言板功能
+    Route::prefix('message')->group(function () {
+        Route::post('add', [MessageController::class, 'store']);
+        Route::get('index', [MessageController::class, 'index']);
+        Route::post('delete', [MessageController::class, 'destroy']);
+        Route::post('edit', [MessageController::class, 'edit']);
+        Route::get('find', [MessageController::class, 'show']);
+
+        Route::prefix('reply')->group(function () {
+            Route::post('add', [MessageController::class, 'replyStore']);
+        });
+    });
+    // Route::resource('products', ProductController::class);
 });

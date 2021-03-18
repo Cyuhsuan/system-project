ffa24abce1a5afe08760b599e983cf9f98725851
yu <yu@gmail.com>
2021-03-18 17:24:15 +0800

    [add] new web

--- public/web/service-worker.js
+++ public/web/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web/precache-manifest.458bb07f17de2830cf9df85d164a8816.js"
+  "/web/precache-manifest.a50c9f5e4a224717a3905a4bb6a74158.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "system_project_1"});
